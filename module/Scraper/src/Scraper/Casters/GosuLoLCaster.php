25c3606d897162ef1d4ed3b98febdd7fd445278c
jr-cobweb <j.richardson@cobwebinfo.com>
2016-02-29 16:36:27 +0000

    DocBlock amends

--- module/Scraper/src/Scraper/Casters/GosuLoLCaster.php
+++ module/Scraper/src/Scraper/Casters/GosuLoLCaster.php
@@ -1,9 +1,7 @@
 <?php namespace Scraper\Casters;
 
 use \Doctrine\ORM\EntityRepository;
-use Matches\Entity\Event;
 use Matches\Entity\Match;
-use Matches\Entity\Sport;
 use Matches\Entity\Team;
 use Scraper\Casters\Contract\Caster;
 
@@ -30,16 +28,24 @@ class GosuLoLCaster implements Caster
     protected $entities;
 
     /**
+     * @var \DateTime
+     */
+    protected $startTime;
+
+    /**
      * GosuLoLCaster constructor.
      * @param EntityRepository $matchRepo
      * @param EntityRepository $teamRepo
-     * @todo - fix static event/sport.
      */
     public function __construct(\Doctrine\ORM\EntityManager $em) {
         $this->em = $em;
 
         $this->matchRepo = $this->em->getRepository('Matches\Entity\Match');
         $this->teamRepo = $this->em->getRepository('Matches\Entity\Team');
+
+        $this->startTime = $now = new \DateTime();
+
+        $this->em->clear();
     }
 
     /**
@@ -52,44 +58,104 @@ class GosuLoLCaster implements Caster
 
     /**
      * @param array $data
+     * @return null
      */
     public function cast(array $data)
     {
-        $this->em->clear();
-
         $sport = $this->em->getRepository('Matches\Entity\Sport')
-            ->find( 1);
+            ->find(1);
 
-        $event = $this->em->getRepository('Matches\Entity\Event')
-            ->find( 1);
+        $event = $this->getEvent($data['event']);
 
-        $now = new \DateTime();
+        $first_team = $this->getTeam($data['first_team']);
+        $second_team = $this->getTeam($data['second_team']);
+        $date = $this->timeRemainingToDate($data['date']);
 
-        foreach($data as $key=>$matchData) {
+        $match = new Match();
+        $match->setFirstTeam($first_team);
+        $match->setSecondTeam($second_team);
+        $match->setSport($sport);
+        $match->setCreatedAt($this->startTime);
+        $match->setUpdatedAt($this->startTime);
+        $match->setWinner(0);
+        $match->setDate($date);
+        $match->setEvent($event);
 
-            $first_team = $this->getTeam($matchData['opponent1']);
-            $second_team = $this->getTeam($matchData['opponent2']);
+        $this->entities[] = $match;
+    }
 
-            $match = new Match();
-            $match->setFirstTeam($first_team);
-            $match->setSecondTeam($second_team);
-            $match->setSport($sport);
-            $match->setCreatedAt($now);
-            $match->setUpdatedAt($now);
-            $match->setWinner(0);
-
-            //todo - fix date and event
-            //todo - check if match already existst, hash objecT?
-            //todo - properly handle persitence and validation.
-            $match->setEvent($event);
-            $match->setDate($now);
-
-            $this->entities[] = $match;
-
-            $this->create($match);
+    /**
+     * @param $date
+     * @return \DateTime|null
+     */
+    protected function timeRemainingToDate($date) {
+        if($date == false) {
+            return;
         }
 
-        $this->em->flush();
+        $startTime = $this->startTime->format("Y-m-d H:i:s");
+
+        $dateTime = new \DateTime($startTime);
+
+        $countDownParts = explode(' ', $date);
+
+        if(count($countDownParts) == 0) {
+            return;
+        }
+
+        foreach($countDownParts as $interval) {
+            $interval = $this->formatInterval($interval);
+
+            if(!$interval)
+                break;
+
+            $dateTime->modify('+' . $interval);
+        }
+
+        return $dateTime;
+    }
+
+    /**
+     * Converts shorthand time intervals to
+     * DateTime supported values (e.g 'd' to 'days')
+     *
+     * @param $interval
+     * @return string|void
+     */
+    protected function formatInterval($interval) {
+        if(strpos($interval, 'd')) {
+            $period = ' days';
+        }
+        elseif(strpos($interval, 'h')) {
+            $period = ' hours';
+        }
+        elseif(strpos($interval, 'm')) {
+            $period = ' minutes';
+        }
+        elseif(strpos($interval, 's')) {
+            $period = ' seconds';
+        }
+        else {
+            return;
+        }
+
+        return intval($interval) . $period;
+    }
+
+    /**
+     * @param $source
+     * @return mixed
+     */
+    protected function getEvent($source) {
+        $repo = $this->em->getRepository('Matches\Entity\Event');
+
+        $event = $repo->findEventBySource($source);
+
+        if(!$event) {
+            $event = $repo->findDefault();
+        }
+
+        return $event;
     }
 
     /**
@@ -105,17 +171,9 @@ class GosuLoLCaster implements Caster
             $team->setName($name);
             $team->setSport(1);
 
-            $this->create($team);
+            $this->em->persist($team);
         }
 
         return $team;
-    }
-
-    /**
-     * @param $entity
-     * @todo move to service (validate first)
-     */
-    protected function create($entity) {
-        $this->em->persist($entity);
     }
 }
